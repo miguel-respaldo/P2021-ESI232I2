e3745a31316210c2fec15217766f4ccf132d3269
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-09 14:47:21 -0600

    concatenación y repeteción

--- 0209 entradas salidas y cadenas/cadenas de texto.py
+++ 0209 entradas salidas y cadenas/cadenas de texto.py
@@ -13,3 +13,25 @@ print(s4)
 print(s4[3]) # Imprime la letra r
 print(s4[6]) # Imprime la letra a
 print(s4[0]) # Imprime la letra L
+
+# Concatenación
+
+s1="Algoritmos"
+s2="Programación"
+
+s = s1 + " y " + s2
+# "áéàû*uÇḉ"
+print(s)
+
+s3 = "Algoritmos" + " y " + "Programación"
+print(s3)
+s4 = "Algoritmos y Programación"
+print(s4)
+
+# Repeticiones en cadenas
+
+s3 = 3*s1
+print(s3)
+# ahora con espacios
+s3 = 3*(s1 + " ")
+print("con espacios", s3)
