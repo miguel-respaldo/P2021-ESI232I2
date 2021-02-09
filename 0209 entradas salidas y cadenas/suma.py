8ccf6b6ea2c8f6f446cc6a5613993209feb041e9
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-09 13:59:38 -0600

    hacemos la suma

---
+++ 0209 entradas salidas y cadenas/suma.py
@@ -0,0 +1,8 @@
+#  Leer el primer número
+num1 = eval(input("Ingresa el primer número: "))
+#  Leer el segundo número
+num2 = eval(input("Ingresa el segundo número: "))
+#  Hacer la operación
+resultado = num1 + num2
+#  Mostrabamos el resultado
+print("El resultado es:", resultado)
