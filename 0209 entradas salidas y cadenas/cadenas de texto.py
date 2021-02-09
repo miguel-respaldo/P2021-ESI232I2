bbb990dc26b6441f7469d396b398075ef1c5a24c
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-09 14:37:05 -0600

    iniciamos con cadenas

---
+++ 0209 entradas salidas y cadenas/cadenas de texto.py
@@ -0,0 +1,15 @@
+# Inicializamos las cadenas de texo
+# s viene de String -> cadena de texto
+s1 = str() # Esto es una cadena de texto vacia
+s2 = ""    # Esto es una cadena de texto vacia
+s3 = "Libreta"  # contiene la palabra Libreta
+s4 = str("Libreta") # Lo mismo que s3
+# Imprimimos s4
+print(s4)
+
+# Libreta
+# 0123456  <-- indices
+
+print(s4[3]) # Imprime la letra r
+print(s4[6]) # Imprime la letra a
+print(s4[0]) # Imprime la letra L
