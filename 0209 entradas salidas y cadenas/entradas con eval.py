6e2ce1e0a21cfd062eddfe3a7af603d8cf746f18
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-09 13:42:45 -0600

    entradas con eval texto

--- 0209 entradas salidas y cadenas/entradas con eval.py
+++ 0209 entradas salidas y cadenas/entradas con eval.py
@@ -4,4 +4,4 @@ valor = eval(input("Dame un valor: "))
 
 # Mostrar en pantalla
 print("El valor ingresado es:", valor)
-print("El tipo de dato de valor es:", type(valor))
+print("El tipo de dato de valor es (con funcion eval):", type(valor))
