f1189964014e26c511585852663bbcbfd6c65e01
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-12 14:24:56 -0600

    funcion strip

--- 0212  - cadenas de texto/03_subcadenas.py
+++ 0212  - cadenas de texto/03_subcadenas.py
@@ -1,7 +1,13 @@
 s = 'Bienvenido a Python'
-#    1234567890123456789
+#    0123456789012345678
 print(s.endswith("thon"))
 print(s.startswith("Buen"))
 print(s.find("ido"))
 print(s.rfind("venida"))
 print(s.count("o"))
+
+# Eliminando espacio en blanco
+s = "  Hola Mundo   "
+print("--" + s.lstrip()+ "---")
+print("--" + s.rstrip()+ "---")
+print("--" + s.strip() + "---")
