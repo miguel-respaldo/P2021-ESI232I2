2a0fe0b7c1dfde86de099a7f25dd4554a8650b5c
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-12 13:28:58 -0600

    operadores relacioneales

--- 0212  - cadenas de texto/operaciones cadenas.py
+++ 0212  - cadenas de texto/operaciones cadenas.py
@@ -4,3 +4,13 @@ s2="Programación"
 
 print("algo" in s1)
 print("gramo" not in s2)
+
+#Los operadores  relacionales  nos permiten
+# comparar  dos cadenas de texto.
+
+print("asa" < "oso")
+print("cosa" <= "cosas")
+print("CASA" >= "casa")
+print("casas">= "casa")
+print("hola" == "HOLA")
+print("mama" != "papa")
