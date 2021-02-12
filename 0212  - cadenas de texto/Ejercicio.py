3715a017a956611aaa460740c4fa4de6edbc2384
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-12 14:31:23 -0600

    ejercicio

---
+++ 0212  - cadenas de texto/Ejercicio.py
@@ -0,0 +1,10 @@
+# Contador de letras.
+# Contar las vocales  de una cadena de texto.
+# No considerar  vocales  con acento.
+# Ejemplo:
+# Entrada: Programacion
+# Salida: 2 a
+#         0 e
+#         1 i
+#         2 o
+#         0 u
