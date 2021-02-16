e71654945a922ecf4b0cd4ebfa67479e94e262c2
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-16 14:00:22 -0600

    ahora en python con lower

--- 0216 - Sentencia Si/01_colores.py
+++ 0216 - Sentencia Si/01_colores.py
@@ -4,10 +4,11 @@
 #Leer color
 color = input("¿Qué color te gusta?: ")
 
-#color_min < - Minusculas(color)
+#color_min <- Minusculas(color)
+color_min = color.lower()
 
 #Si color_min = "rojo" Entonces
-if color == "rojo":
+if color_min == "rojo":
 #  Escribir "A mi tambien me gusta el rojo"
    print("A mi también me gusta el rojo.")
 #Fin Si
--- 0216 - Sentencia Si/01_colores_sin_comentarios.py
+++ 0216 - Sentencia Si/01_colores_sin_comentarios.py
@@ -1,5 +1,7 @@
 
 color = input("¿Qué color te gusta?: ")
 
+color_min = color.lower()
+
 if color == "rojo":
    print("A mi también me gusta el rojo.")
