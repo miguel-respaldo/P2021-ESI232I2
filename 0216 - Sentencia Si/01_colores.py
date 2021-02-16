3c0f5149c8ea590ccf965ef3c79f380849ad6783
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-16 13:56:06 -0600

    ahora en python

---
+++ 0216 - Sentencia Si/01_colores.py
@@ -0,0 +1,15 @@
+# Algoritmo colores
+
+#Escribir "¿Que color te gusta?: " Sin Saltar
+#Leer color
+color = input("¿Qué color te gusta?: ")
+
+#color_min < - Minusculas(color)
+
+#Si color_min = "rojo" Entonces
+if color == "rojo":
+#  Escribir "A mi tambien me gusta el rojo"
+   print("A mi también me gusta el rojo.")
+#Fin Si
+
+#FinAlgoritmo
---
+++ 0216 - Sentencia Si/01_colores_sin_comentarios.py
@@ -0,0 +1,5 @@
+
+color = input("¿Qué color te gusta?: ")
+
+if color == "rojo":
+   print("A mi también me gusta el rojo.")
