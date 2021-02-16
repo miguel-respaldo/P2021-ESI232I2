30f47aa0e4e78200c57bd7d535250e75afd7a393
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-16 14:22:16 -0600

    par o impar

---
+++ 0216 - Sentencia Si/02_par_o_impar.psc
@@ -0,0 +1,15 @@
+Algoritmo par_o_impar
+	
+	Escribir "Introduce un número: " Sin Saltar
+	Leer num
+	
+	//Escribir "el residuo es: ", num MOD 2
+	//Escribir "el residuo es: ", num % 2
+	
+	Si num % 2 = 0 Entonces
+		Escribir "El número es par"
+	SiNo
+		Escribir "El número es impar"
+	Fin Si
+	
+FinAlgoritmo
---
+++ 0216 - Sentencia Si/02_par_o_impar.py
@@ -0,0 +1,15 @@
+#Escribir "Introduce un n�mero: "Sin Saltar
+#Leer num
+
+num = input("Introduce un número: ")
+
+#Si num % 2 = 0 Entonces
+#   Escribir "El n�mero es par"
+#SiNo
+#   Escribir "El n�mero es impar"
+#FinSi
+
+if num % 2 == 0:
+   print("El número es par")
+else:
+   print("El número es impar")
