9219bf6920362c7c08f989cb0ee0cc50de8002f1
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-26 13:37:27 -0600

    inciamos ciclo for

---
+++ 0226 - Ciclo for/01_del_1_al_10.psc
@@ -0,0 +1,7 @@
+Algoritmo contar_del_1_al_10
+	
+	Para num <- 1 Hasta 10 Con Paso 1 Hacer
+		Escribir "El numero es: ", num
+	Fin Para
+	
+FinAlgoritmo
---
+++ 0226 - Ciclo for/01_del_1_al_10.py
@@ -0,0 +1,8 @@
+# Imprimimos del 1 al 10
+#for num in rage(1,10+1,1):
+for num in range(1, 11, 1):
+    print("El número es:",num)
+
+#  rage(X,Y,Z)
+# Intervalo semi-abierto [X,Y)
+# [1,5) = 1,2,3,4
