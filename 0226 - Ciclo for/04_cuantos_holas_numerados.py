ab296e54476ec2d9cf23153ca3001c4b266a6b26
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-02-26 13:57:12 -0600

    inciamos ciclo for 3

---
+++ 0226 - Ciclo for/04_cuantos_holas_numerados.py
@@ -0,0 +1,7 @@
+# Escribir la palabra "hola" en número de veces
+# que el usuario nos indique
+
+cuantos = int(input("¿Cuantos holas quieres?: "))
+
+for contador in range(cuantos):
+    print(contador,": Hola")
