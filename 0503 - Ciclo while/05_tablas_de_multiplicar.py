94f5cd3e70f796bd0e8afb5906c8a3fc863f4057
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-03-05 14:31:13 -0600

    tablas de multiplicar while

---
+++ 0503 - Ciclo while/05_tablas_de_multiplicar.py
@@ -0,0 +1,11 @@
+# Imprimir las tablas de multiplicar
+
+tabla = int(input("¿Cual tabla de multiplicar quieres?: "))
+
+i = 1
+#for i in range(1,11):
+#while i < 11:
+while i <= 10:
+    print(i," x ",tabla," = ",tabla * i)
+    i = i + 1
+
