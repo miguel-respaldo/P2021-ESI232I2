b3d61be4fe672cf03c7ae2fd029eacbed2086f47
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-03-12 13:44:21 -0600

    uso de break

---
+++ 0312 - ciclos/01_uso_de_break_en_for.py
@@ -0,0 +1,6 @@
+# Usar el break en un for del 1 al 10
+
+for i in range(1,101):
+    print(i)
+    if i == 10:
+        break
---
+++ 0312 - ciclos/01_uso_de_break_while.py
@@ -0,0 +1,12 @@
+# Usar el break en un while del 1 al 10
+
+i = 1
+
+while i<=100:
+    print(i)
+    if i == 10:
+        print("es 10")
+        break
+    i = i +1
+
+print("Terminamos")
