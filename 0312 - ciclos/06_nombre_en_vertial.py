6fa771268eb9db542721a89fcaa94ee1acded7d1
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-03-12 14:47:00 -0600

    ejercicio 05 resuelto 2

---
+++ 0312 - ciclos/06_nombre_en_vertial.py
@@ -0,0 +1,14 @@
+# Ingresar un nombre y desplegarlo de forma vertial
+# Ejemplo:
+#   Ingresa un nombre: Python
+#   P
+#   y
+#   t
+#   h
+#   o
+#   n
+
+nombre = input("Ingresa un nombre: ")
+
+for i in nombre:
+    print(i)
