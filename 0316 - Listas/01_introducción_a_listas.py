fd928d766e8acaa8ca21ba2ab1cdf53ae56ea1fe
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-03-16 13:44:53 -0600

    introducion listas

---
+++ 0316 - Listas/01_introducción_a_listas.py
@@ -0,0 +1,12 @@
+lista_1 = list() # crea una lista vacía
+lista_2 = list([2, 3, 4]) #crea una lista con los elementos 2, 3, 4
+lista_3 = list(["rojo", "verde", "azul"]) # Crea lista con cadena de texto
+lista_4 = list(range(3, 6))# crea una lista con los elementos 3, 4, 5
+lista_5 = list("abcd") # crea una lista con los elementos a, b, c, d
+lista_6 = list([2, "tres", 4.5]) # lista con elementos de diferentes tipos.
+
+lista_7 = []
+lista_8 = [2, 3, 4]
+lista_9 = ["rojo", "verde", "azul"]
+
+print(lista_2[0])
