9262b5b2eed2d2413aa335a4886c7262dfa41395
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-03-23 14:40:42 -0600

    formato y total

--- 0323 - Tiendita/01_tiendita.py
+++ 0323 - Tiendita/01_tiendita.py
@@ -1,7 +1,7 @@
 # Lista de los productos
-lista_productos = ["papas", "refrescos", "tortillas", "jamon"]
+lista_productos = ["papas", "refrescos", "tortillas", "jamon", "mazapan"]
 # Lista de los precios
-lista_precio =    [12,      10.5,         20,         40]
+lista_precio =    [12,      10.5,         20,         40,  5,]
 # Lista del carrito con los productos
 carrito_producto = []
 # Lista de la cantidad en cada producto
@@ -19,7 +19,7 @@ while opcion != 6:
     print("2. Mostrar el carrito")
     print("3. Agregar al carrito")
     print("4. Borrar un producto del carrito")
-    print("5. Pagar productos")
+    print("5. Cantidad a pagar")
     print("6. Salir")
     print("")
     opcion = int(input("Opción: "))
@@ -27,20 +27,21 @@ while opcion != 6:
 
     if opcion == 1:
         tamano = len(lista_productos)
-        print("No.    Nombre     Precio")
+        #print("No.      Nombre       Precio")
+        print("{:<5s}{:<15s}{:>8s}".format("No.","Nombre","Precio"))
         for i in range(tamano):
-            print(i+1,". ",lista_productos[i],"  ", lista_precio[i])
+            print("{:<5d}{:<15s}{:>8.2f}".format(i+1,lista_productos[i],lista_precio[i]))
 
     elif opcion == 2:
         tamano = len(carrito_producto)
         print("----  Carrito ----------")
-        print("No.    Nombre     Precio      Cantidad")
+        print("No.      Nombre         Precio         Cantidad")
         for i in range(tamano):
             print(i+1,". ", lista_productos[ carrito_producto[i] ],"  ",lista_precio[ carrito_producto[i] ], "   ", carrito_cantidad[i])
 
     elif opcion == 3:
         tamano = len(lista_productos)
-        print("No.    Nombre     Precio")
+        print("No.      Nombre       Precio")
         for i in range(tamano):
             print(i+1,". ",lista_productos[i],"  ", lista_precio[i])
         print("------------------------")
@@ -52,12 +53,12 @@ while opcion != 6:
     elif opcion == 4:
         tamano = len(carrito_producto)
         print("----  Carrito ----------")
-        print("No.    Nombre     Precio      Cantidad")
+        print("No.     Nombre      Precio       Cantidad")
         for i in range(tamano):
-            print(i+1,". ", lista_productos[ carrito_producto[i] ],"  ",lista_precio[ carrito_producto[i] ], "   ", carrito_cantidad[i])
+            print(i+1,". ", lista_productos[ carrito_producto[i] ],"    ",lista_precio[ carrito_producto[i] ], "   ", carrito_cantidad[i])
         print("------------------------")
         # Leemos el numero del carrito a borrar
-        num_producto = int(input("Ingresa el número del producto a borrar"))
+        num_producto = int(input("Ingresa el número del producto a borrar: "))
         # Sustituir por un 0 el valor del articulo a borrar
         carrito_producto[ num_producto-1 ] = -1
         carrito_cantidad[ num_producto-1 ] = -1
@@ -65,18 +66,16 @@ while opcion != 6:
         carrito_producto.remove(-1)
         carrito_cantidad.remove(-1)
 
-
-
-
-
-
-
-
-
     elif opcion == 5:
-        print("La opción es 5")
+        total = 0.0
+        tamano = len(carrito_producto)
+        for i in range(tamano):
+            total = total + lista_precio[ carrito_producto[i] ] * carrito_cantidad[i]
+        print("La cantidad total a pagar es: ", total)
+        print("----------------------------------------")
+
     elif opcion == 6:
-        print("La opción es 6")
+        print("Gracias por usar el programa")
     else:
         print("La opción no existe.")
 
