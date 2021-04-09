4094d872f509624637da2be006180ee3b9678bcc
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-04-09 14:38:19 -0500

    triangulo

--- 0409 - funciones/04_ejercicio_areas.py
+++ 0409 - funciones/04_ejercicio_areas.py
@@ -6,15 +6,30 @@ def menu():
     print("4) Calcular el área de un círculo")
     print("5) Salir")
 
+
 def area_rectangulo(base, altura):
     resultado = base * altura
     return resultado
+
+
+def area_triangulo(base, altura):
+    resultado = base * altura / 2
+    return resultado
+
 
 def calcular_area_rectangulo(figura):
     largo = float(input("¿Cuanto mide de largo?: "))
     ancho = float(input("¿Cuanto mide de ancho?: "))
     area = area_rectangulo(largo, ancho)
     print("El area del",figura," es ", area)
+
+
+def calcular_area_triangulo():
+    base = float(input("¿Cuanto mide de base?: "))
+    altura = float(input("¿Cuanto mide de altura?: "))
+    area = area_triangulo(base, altura)
+    print("El area del triangulo es ", area)
+
 
 opcion = 0
 # Mientras la opcion sea deferente de 5
@@ -24,7 +39,7 @@ while opcion != 5:
     if opcion == 1:
         calcular_area_rectangulo("rectangulo")
     elif opcion == 2:
-        print("opcion 2")
+        calcular_area_triangulo()
     elif opcion == 3:
         calcular_area_rectangulo("cuadrado")
     elif opcion == 4:
