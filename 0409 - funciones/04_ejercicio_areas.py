6f7a2b5588e1aa9a3f226746235b9603094ac526
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-04-09 14:33:36 -0500

    cuadrado parametro

--- 0409 - funciones/04_ejercicio_areas.py
+++ 0409 - funciones/04_ejercicio_areas.py
@@ -10,11 +10,11 @@ def area_rectangulo(base, altura):
     resultado = base * altura
     return resultado
 
-def calcular_area_rectangulo():
+def calcular_area_rectangulo(figura):
     largo = float(input("¿Cuanto mide de largo?: "))
     ancho = float(input("¿Cuanto mide de ancho?: "))
     area = area_rectangulo(largo, ancho)
-    print("El area del rectangulo es ", area)
+    print("El area del",figura," es ", area)
 
 opcion = 0
 # Mientras la opcion sea deferente de 5
@@ -22,11 +22,11 @@ while opcion != 5:
     menu()
     opcion = int(input("Opción: "))
     if opcion == 1:
-        calcular_area_rectangulo()
+        calcular_area_rectangulo("rectangulo")
     elif opcion == 2:
         print("opcion 2")
     elif opcion == 3:
-        calcular_area_rectangulo()
+        calcular_area_rectangulo("cuadrado")
     elif opcion == 4:
         print("opcion 4")
 
