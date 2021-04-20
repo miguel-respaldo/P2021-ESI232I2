b0654c3b6fdc41eab03a4c8f59f0df5f272771b6
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-04-20 14:29:33 -0500

    promedio materias

--- 0420 - Archivos CSV/02_promedios.py
+++ 0420 - Archivos CSV/02_promedios.py
@@ -1,6 +1,25 @@
 # CSV -> Comma Separated Values
 import csv
 
+def promedio_por_materia(lector):
+    lista_materias = []
+    lista_promedio = []
+    contador_alumnos = 0
+    for fila in lector:
+        if fila[0] == "nombre":
+            for i in range(len(fila)-1):
+                lista_materias.append(fila[i+1])
+                lista_promedio.append(0.0)
+        else:
+            for i in range(len(fila)-1):
+                lista_promedio[i] = lista_promedio[i] + float(fila[i+1])
+            contador_alumnos = contador_alumnos +1
+    for i in range(contador_alumnos):
+        lista_promedio[i] = lista_promedio[i] / contador_alumnos
+    for i in range(len(lista_materias)):
+        print("El promedio de la materia de {:<17s} es {:>5.2f}",lista_materias[i], lista_promedio[i])
+
+
 def promedio_por_alumno(lector):
     for fila in lector:
         promedio = 0.0
@@ -18,4 +37,10 @@ archivo = open(nombre_archivo,"r")
 lector = csv.reader(archivo)
 
 promedio_por_alumno(lector)
+
+# Uso la funcion seek para irme al principio del archivo
+archivo.seek(0)
+
+promedio_por_materia(lector)
+
 
