2bf62249832b0e15618db39a91ca8daeffb4cff0
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2021-05-07 14:00:04 -0500

    archivo fix 2

--- 0416 - archivos/02_open_w.py
+++ 0416 - archivos/02_open_w.py
@@ -1,6 +1,6 @@
 #archivo = open("prueba2.txt","w")
 archivo = open("prueba2.txt","wt")
-linea_escrita = archivo.write("Estes es un nuevo Archivo")
+linea_escrita = archivo.write("Estes es un nuevo Archivo\n")
 
 archivo.write("{:<5s}{:<15s}{:>8s}\n".format("No.", "Nombre", "Precio"))
 print("{:<5s}{:<15s}{:>8s}".format("No.", "Nombre", "Precio"))
